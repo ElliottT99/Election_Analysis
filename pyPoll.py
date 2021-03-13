ad45468b66cb79fcf7c5aafe4f532f0bb23add50
ElliottT99 <elliott.tarnovsky99@gmail.com>
2021-03-12 21:18:42 -0500

    Adding the election audit code

--- pyPoll.py
+++ pyPoll.py
@@ -5,8 +5,8 @@ import os
 
 #accessing .csv file to read the results
 
-file_to_load = os.path.join("Resources", "election_results.csv")
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_save = os.path.join("..", "analysis", "election_analysis.txt")
 
 #testing writing to files
 #outfile = open(file_to_save, "w")
@@ -17,20 +17,61 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 #method one for reading data from csv
 #election_data = open(file_to_load, r)
 
+#initializing variables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #method two for reading data from csv
 
 #with open(file_to_save, "w") as txt_file:
 #    txt_file.write("Counties in the Election\n---------------------\nArapahoe\nDenver\nJefferson")
 
-with open(file_to_load, "r") as election_data:
+with open(file_to_load) as election_data:
     #to do: analyze data
     file_reader = csv.reader(election_data)
-    headers = next(file_reader)
-    print(headers)
-#    for row in file_reader:
-#        print(row)
+    
+    #headers = next(file_reader)
+    #print(headers)
+    
+    for row in file_reader:
+        #prints each row in csv
+        #print(row)
+        #add to the total vote count
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        candidate_votes[candidate_name] += 1
+    
+    for candidate_name in candidate_votes:
+        votes = candidate_votes[candidate_name]
+        votes_percentage = float(votes)/float(total_votes) * 100
+        #print(f'{candidate_name} recieved {votes_percentage:.3}% of the votes')
 
+        if (votes > winning_count) and (votes_percentage > winning_percentage):
+         winning_count = votes
+         winning_percentage = votes_percentage
+         winning_candidate = candidate_name
+        print(f"{candidate_name}: {votes_percentage:.1f}% ({votes:,})\n")
 
+#print(total_votes)
+#print(candadite_options)
+#print(candadite_votes)
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 #1. total number of votes
 #2. list of candadites who recieved votes
